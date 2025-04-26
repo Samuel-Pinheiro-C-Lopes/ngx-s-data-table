a6a8030438d47f9e25ed15c1844481f73742486b
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-04-26 16:07:29 -0300

    Base module for the library created

---
+++ src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ngx-s-data-table
+ */
+
+/*
+export * from './lib/ngx-s-data-table.service';
+export * from './lib/ngx-s-data-table.component';
+*/
+export { SDataTableModule } from './lib/s-data-table.module';
+
