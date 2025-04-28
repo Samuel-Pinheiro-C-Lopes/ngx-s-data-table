e0ddd2a2b6f7027edf440e98f166b075e5c66bf3
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-04-28 02:10:31 -0300

    updated public API export

--- src/public-api.ts
+++ src/public-api.ts
@@ -7,7 +7,7 @@ export * from './lib/ngx-s-data-table.service';
 export * from './lib/ngx-s-data-table.component';
 */
 
-export { PropertyMapping } from './lib/classes/property-mapping.class';
-export { DataTableComponent } from './lib/component/data-table.component';
-export { DataTableModule } from './lib/s-data-table.module';
+export * from './lib/classes/property-mapping.class';
+export * from './lib/component/data-table.component';
+export * from './lib/s-data-table.module';
 
